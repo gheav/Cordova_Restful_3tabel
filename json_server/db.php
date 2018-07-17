19edb08acc48dfecf4264a6bce1a7ad2414b75de
gheav <38318210+gheav@users.noreply.github.com>
2018-07-17 16:26:57 +0700

    Update db.php

--- json_server/db.php
+++ json_server/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","root","","gheav_appmob") or die ("could not connect database");
+ $con = mysqli_connect("156.67.215.51","gheav","heavy1808","gheav_appmob") or die ("could not connect database");
 ?>
